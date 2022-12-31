a121611493f1390f8df68be1348ade4d67f77e24
Darío <43958610+satcheng@users.noreply.github.com>
2023-01-01 00:43:32 +0100

    Update bisiesto.py

--- bisiesto.py
+++ bisiesto.py
@@ -1,17 +1,35 @@
 import time
 
-class años():
+class extractCaracteristicsOfYears():
 
-    def bisiesto(year):
+    def isLeapYear(year):
 
-        t = (year, 12, 31, 0, 0, 0, 0, 0, 0)
-        secs = time.mktime( t )
+        if type(year) is not int:
 
-        e = time.localtime(secs)
+            print("Error 1: El año debe ser tipo entero")
 
-        if e.tm_yday == 366:
-            print("Es año bisiesto")
+        elif year < 1900:
+
+            print("Error 2: El año debe ser mayor que 1900")
+
+        elif year >= 2200:
+
+            print("Error 3: El año debe ser menor que 2200")
+
         else:
-            print("No es año bisiesto")
 
-años.bisiesto(2011)
+            defineYear = (year, 12, 31, 0, 0, 0, 0, 0, 0)
+
+            descomposedYearAttributes = time.localtime(time.mktime(defineYear))
+
+            if descomposedYearAttributes.tm_yday == 366:
+
+                print("Es año bisiesto")
+
+            else:
+
+                print("No es año bisiesto")
+
+yearToAnalyse = int(input("Introduce el año que quieras saber: "))
+
+extractCaracteristicsOfYears.isLeapYear(yearToAnalyse)
