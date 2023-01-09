ea38f3e79c608566b600330381583fcaa20b0705
Darío <43958610+satcheng@users.noreply.github.com>
2023-01-09 18:07:27 +0100

    Create alumno.py

---
+++ alumno.py
@@ -0,0 +1,13 @@
+class Alumno():
+
+    def __init__(self, Nombre, Nota):
+        
+        if Nota >= 5:
+
+            print("El alumno", Nombre, "ha aprobado.")
+
+        else:
+        
+             print("El alumno", Nombre, "ha suspendido.")
+
+Alumno("Paco",9.4)
